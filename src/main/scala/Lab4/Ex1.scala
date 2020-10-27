ea4d833500c374db3fb4f737358fa1ab2176dbda
rox <cernatmonicaroxana@gmail.com>
2020-10-27 10:15:08 +0200

    Hello World program

--- src/main/scala/Lab4/Ex1.scala
+++ src/main/scala/Lab4/Ex1.scala
@@ -1,15 +1,51 @@
 package Lab4 
 
+import com.cra.figaro.algorithm.factored.VariableElimination
 import com.cra.figaro.language._
 import com.cra.figaro.algorithm.sampling._
+import com.cra.figaro.library.compound.If
 
 object Ex1 {
-	def main(args: Array[String]) {
-		val test = Constant("Test")
+	val sunnyToday = Flip(0.2)
 
-		val algorithm = Importance(1000, test)
-		algorithm.start()
-		
-		println(algorithm.probability(test, "Test"))
+	val greetingToday = If (
+		sunnyToday,
+		Select(0.6 -> "Hello, world!", 0.4 -> "Howdy, universe!"),
+		Select(0.2 -> "Hello, world!", 0.8 -> "Oh no, not again")
+		)
+
+	val sunnyTomorrow = If(sunnyToday, Flip(0.8), Flip(0.05))
+
+	val greetingTomorrow = If (
+		sunnyTomorrow,
+		Select(0.6 -> "Hello, world!", 0.4 -> "Howdy, universe!"),
+		Select(0.2 -> "Hello, world!", 0.8 -> "Oh no, not again")
+		)
+
+	def predict() {
+		val result = VariableElimination.probability(greetingToday, "Hello, world!")
+		println("Today’s greeting is \"Hello, world!\" " +
+			"with probability " + result + ".")
 	}
+
+	def infer() {
+		greetingToday.observe("Hello, world!")
+		val result = VariableElimination.probability(sunnyToday, true)
+		println("If today's greeting is \"Hello, world!\", today’s " + "weather is sunny with probability " + result + ".")
+	}
+
+	def learnAndPredict() {
+		greetingToday.observe("Hello, world!")
+		val result = VariableElimination.probability(greetingTomorrow, "Hello, world!")
+		println("If today's greeting is \"Hello, world!\", " + "tomorrow's greeting will be \"Hello, world!\" " +
+			"with probability " + result + ".")
+	}
+
+
+	def main(args: Array[String]) {
+		predict()
+		infer()
+		learnAndPredict()
+	}
+
 }
